5abc4cbf0284dcef36b59f1e7305aa2e39e876e8
arnoldsanders <arnoldsander@gmail.com>
2023-09-03 20:34:24 -0400

    bad code clearly needs refactor

--- employee.ts
+++ employee.ts
@@ -22,16 +22,44 @@ import Person from './person';
  class Employee extends Person {
     private deptName : string;
     private static numEmployees : number = 0;
+    public employeeID: number  = 0;
 
     constructor ();
     constructor (deptName: string);
     constructor (name: string, birthYear: number, deptName: string);
     constructor (...args : any[]) {
        super(); //apparantly when extending super has to be called first before 'this'.
+       Employee.numEmployees += 1;
+        
        if (args.length === 0) {
         this.deptName = "";
-        Employee.numEmployees += 1;
+        this.employeeID += 1;
+       }
+       if (args.length === 1 ) {
+            if (typeof args[0] !== "string") {
+                throw new Error("must pass a string value");
+            }
+
+        this.deptName = args[0];
+        this.employeeID += 1; 
        }
 
+       if (args.length === 3) {
+        if (typeof args[0] !== "string") {
+            throw new Error("must pass a string value");
+        }
+        if (typeof args[1] !== "number") {
+            throw new Error("must pass a string value");
+        }
+        if (typeof args[2] !== "number") {
+            throw new Error("must pass a string value");
+        }
+            this.name = args[0];
+            this.birthYear = args[1]
+            this.deptName = args[2];
+            this.employeeID += 1; 
+        }
+       
     }
+
  }
